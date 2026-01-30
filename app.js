6a38e8c7fb48bd2121b1616283a6b2cce6f42fed
Anirudh Coppisetti <anirudh.coppisetti@rapyder.com>
2026-01-30 22:50:27 +0530

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end("Hello from EKS using bitbucket pipeline 🚀");
+  res.end("Hello from EKS using AWS Codepipeline 🚀");
 }).listen(3000);
